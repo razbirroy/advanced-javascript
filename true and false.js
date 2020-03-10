f80e2bd6874b9b671429abc953ecb8e10dbd0413
Razbir <roybutterfly019@gmail.com>
2020-03-10 20:24:13 +0600

    true and false

--- true and false.js
+++ true and false.js
@@ -1,17 +1,16 @@
-//Falsy
-//0
-//""
-//undefined
-//null
-//NaN
+//Falsy:
+// false
+// 0
+// ""
+// udenfined
+// null
+// NaN
 //Truthy
-//'0'
-
+//'0', ' ' []
 let name = 12;
-if(name || name == 0){
+if(name  || name == 0){
     console.log("condition is true");
 }
-
 else{
     console.log("condition is false");
 }
