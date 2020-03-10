6e014dd3343474447d5821480bdb4d00d3a20ceb
Razbir <roybutterfly019@gmail.com>
2020-03-11 00:45:43 +0600

    map filter

---
+++ map-filter.js
@@ -0,0 +1,27 @@
+const array = [3, 4, 5, 6, 7, 8];
+//const array2 = [];
+
+// for(let i = 0; i < array.length; i++){
+//     const array3 = array[i];
+//     const array4 = array3 * array3;
+//     array2.push(array4);
+//     //console.log(array2);   
+// }
+//console.log(array2); 
+// function square(num){
+//     return num * num;
+//     }
+//const result = element => element* element;
+// const result = array.map(function(num){
+//         return num * num;
+//  });
+//  const result = array.map(x => x*x);
+//  console.log(result);
+
+const bigger = array.filter(x => x>5);
+//const isThere = array.find(x => x > 5);
+
+//console.log(isThere);
+console.log(bigger);
+
+
