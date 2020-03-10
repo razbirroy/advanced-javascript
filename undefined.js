415848d650807a148f28a805afad645982ee0fb1
Razbir <roybutterfly019@gmail.com>
2020-03-10 20:06:05 +0600

    undefined

---
+++ undefined.js
@@ -0,0 +1,11 @@
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const value = add(16);
+console.log(value);
+
+let und = undefined;
+console.log(und);
+
+let ages = [2, 4, 5];
+console.log(ages[11]);
