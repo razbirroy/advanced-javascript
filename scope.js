ba357c4fcb5c471d59efbcbbe90d7b1275dca59a
Razbir <roybutterfly019@gmail.com>
2020-03-13 14:35:57 +0600

    arguments

---
+++ scope.js
@@ -0,0 +1,19 @@
+const digit = 20;
+function sum(num1, num2){
+    const result = num1 + num2 + digit;
+    if(result > 9){
+        var mood = 'happy';
+        mood = 'fishy';
+        mood ='funky';
+        console.log(mood);
+    }
+   // console.log(mood);
+    console.log(result);
+    //return result;
+}
+const add = sum(5, 3);
+//console.log(digit);
+//console.log(num);
+//const num = 'happy';
+//console.log(num);
+//console.log(mood);
